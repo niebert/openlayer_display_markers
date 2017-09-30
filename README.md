46fcc5f410bcd801e68b0be5688a82e5b6c265a7
Engelbert Niehaus <niehaus@uni-landau.de>
2017-09-30 11:53:40 +0200

    First Commit of OpenLayers example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# openlayer_diplay_markers
-The repository contains OpenLayers Example that is able to display a set of icons on a map. The set of georeferrenced icons can be generated with openlayers_selectgeolocation
+# Open Layers: Diplay Markers/Icons on Map
+The repository contains derivative work of the OpenLayers Example  that is able to display a set of icons on a map. The set of georeferrenced icons can be generated with openlayers_selectgeolocation.
+
+## Derivative Works of OpenLayers: Icon Symbolizer
+The repository is based on the [OpenLayers example](http://openlayers.org/en/latest/examples/icon.html)
+
+## LinkParameter Library: linkparam.js
+This library allows to submit the marker data via URL parameter. Furthermore you can submit the zoom factor of the map and the map centre.
+
+## Other related GitHub Project
+* [OpenLayers Select Geolocation](https://github.com/niebert/openlayer_selectlocation) -- [WebApp](https:/niebert.github.io/openlayer_selectlocation) 
+* [Mapper for Sustainable Devlopment Goals](https://github.com/niebert/Mapper4SDG) -- [WebApp](https:/niebert.github.io/Mapper4SDG)
---
+++ docs/js/iconprocessing.js
@@ -0,0 +1,104 @@
+// OpenLayers Derivative Work http://openlayers.org/
+// -------------------------------------------------
+// This library is based on the Open Layers Example of Icon Symbolizer
+// see Icon Symbolizer: http://openlayers.org/en/latest/examples/icon.html
+// Adaption by source by: Engelbert Niehaus
+// repository https://www.github.com/niebert/openlayers_viewicons
+
+
+      var iconStyle = new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: vIconURL
+        }))
+      });
+ 	
+ 	 var iconFeatureArray = [];
+		
+	var vIconImage = new ol.style.Icon(/** @type {olx.style.IconOptions} */ 
+			({
+          		anchor: [0.5, 46],
+          		anchorXUnits: 'fraction',
+          		anchorYUnits: 'pixels',
+          		src: vIconURL
+        	})
+        );
+        	
+      for (var i = 0; i < vIconArray.length; i++) {
+          var iconRecord = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.transform(vIconArray[i].geolocation, 'EPSG:4326', 'EPSG:3857')),
+            name: vIconArray[i].name 
+          });
+          var vIconStyle = new ol.style.Style({
+       		 image: vIconImage
+      	   });
+ 	      iconRecord.setStyle(vIconStyle)
+          iconFeatureArray.push(iconRecord);
+      }
+      
+  	  // Create a c
+      var vectorSource = new ol.source.Vector({
+        features: iconFeatureArray
+      });
+
+      var vectorLayer = new ol.layer.Vector({
+        source: vectorSource
+      });
+
+		
+	  var osm_default =  new ol.layer.Tile({
+    	source: new ol.source.OSM()
+      });
+         
+	  // Create Map with OSM Layer osm_default
+      var map = new ol.Map({
+        layers: [osm_default,vectorLayer],
+        target: document.getElementById('map'),
+        view: new ol.View({
+          center: ol.proj.transform(vMapCenter, 'EPSG:4326', 'EPSG:3857'),
+          zoom: vZoom
+        })
+      });
+      var element = document.getElementById('popup');
+
+      var popup = new ol.Overlay({
+        element: element,
+        positioning: 'bottom-center',
+        stopEvent: false,
+        offset: [0, -50]
+      });
+      map.addOverlay(popup);
+
+      // display popup on click
+      map.on('click', function(evt) {
+        var feature = map.forEachFeatureAtPixel(evt.pixel,
+            function(feature) {
+              return feature;
+            });
+        if (feature) {
+          var coordinates = feature.getGeometry().getCoordinates();
+          popup.setPosition(coordinates);
+          $(element).popover({
+            'placement': 'top',
+            'html': true,
+            'content': feature.get('name')
+          });
+          $(element).popover('show');
+          console.log("Click: "+feature.get('name'));
+        } else {
+          $(element).popover('destroy');
+        }
+      });
+
+      // change mouse cursor when over marker
+      map.on('pointermove', function(e) {
+        if (e.dragging) {
+          $(element).popover('destroy');
+          return;
+        }
+        var pixel = map.getEventPixel(e.originalEvent);
+        var hit = map.hasFeatureAtPixel(pixel);
+        map.getTarget().style.cursor = hit ? 'pointer' : '';
+      });
